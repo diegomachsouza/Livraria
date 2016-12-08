122fba941442a01588c3eb79e098a99ff3948c11
Diego <diegomacsouza@gmail.com>
2016-12-08 21:54:22 -0200

    Adicionado Bundles e configuração da página

---
+++ src/LivrariaBundle/Controller/CaixaController.php
@@ -0,0 +1,25 @@
+<?php
+ 
+
+namespace LivrariaBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Description of CaixaController
+ *
+ * @author aluno
+ */
+class CaixaController extends Controllerlabora
+{
+    /**
+     * @Route("/caixa")
+     */
+    public function pdvAction()
+    {   
+        return $this->render("LivrariaBundle:Caixa:pdv.html.twig");
+    }
+}
--- src/LivrariaBundle/Entity/Produtos.php
+++ src/LivrariaBundle/Entity/Produtos.php
@@ -3,6 +3,7 @@
 namespace LivrariaBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -24,21 +25,28 @@ class Produtos
     
     /**
      * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="Favor informar um nome para o produto")
      */
     private $nome;
     
     /**
      * @ORM\Column(type="integer", length=6)
+     * * @Assert\NotBlank(message="Este campo não pode ficar vazio. Favor informar uma quantidade para o produto")  
+     * @Assert\GreaterThanOrEqual(
+     *      value = 1,
+     *      message="A quantidade deve ser maior que 0")
      */
     private $quantidade;
     
     /**
      * @ORM\Column(type="decimal", scale=2)
+     * * @Assert\NotBlank(message="Favor informar um preço")
      */
     private $preco;
     
     /**
      * @ORM\Column(type="string")
+     * * @Assert\NotBlank(message="Favor informar um tipo")
      */
     private $tipo;
     
@@ -50,8 +58,10 @@ class Produtos
     /**
      * @ORM\ManyToOne(targetEntity="Genero",)
      * @ORM\JoinColumn(name="genero_id", referencedColumnName="id")
+     * * @Assert\NotBlank(message="Carregue um genero")
      */
     private $genero;
+   
 
 
 
